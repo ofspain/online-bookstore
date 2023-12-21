07d707f3e571a16269d4ac67311384613271e2d1
femi <femi@propertypro.ng>
2023-12-21 15:18:03 +0100

    adding cron job to requery pending transactions

--- src/main/java/com/interswitch/bookstore/services/MockTransferService.java
+++ src/main/java/com/interswitch/bookstore/services/MockTransferService.java
@@ -4,11 +4,14 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.interswitch.bookstore.dtos.BankAccountDetail;
 import com.interswitch.bookstore.dtos.BankDetail;
+import com.interswitch.bookstore.dtos.InitializePaymentDTO;
+import com.interswitch.bookstore.models.CartStatus;
 import com.interswitch.bookstore.utils.payment.PaymentDetails;
 import com.interswitch.bookstore.exceptions.PaymentException;
 import com.interswitch.bookstore.models.ShoppingCart;
 import com.interswitch.bookstore.utils.payment.PaymentResponse;
 import com.interswitch.bookstore.utils.payment.transfer.TransferServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -20,12 +23,16 @@ import static com.interswitch.bookstore.utils.BasicUtil.bankOptionToPayFrom;
 import static com.interswitch.bookstore.utils.BasicUtil.bankOptionToPayTo;
 
 @Service("mock_transfer_service")
-public class MockTransferService implements TransferServiceInterface {
+public class MockTransferService extends TransferServiceInterface {
+
+    @Autowired
+    private CartStateMachine cartStateMachine;
 
 
     @Override
-    public PaymentDetails initialize(ShoppingCart shoppingCart) {
-        PaymentDetails paymentDetails = new PaymentDetails(shoppingCart, true, "TRF");
+    public PaymentDetails initialize(InitializePaymentDTO initializePaymentDTO) {
+        PaymentDetails paymentDetails = new PaymentDetails(initializePaymentDTO.getShoppingCart(),
+                true, generateTransactionPrefix());
         paymentDetails.setDetails(new HashMap<>(){{
             put("bank_options",bankOptionToPayTo());
         }});
@@ -77,17 +84,48 @@ public class MockTransferService implements TransferServiceInterface {
 
     @Override
     public String getServiceId() {
+
         return "mock_transfer_service";
     }
 
     @Override
-    public List<BankAccountDetail> getBanksToPayTo() {
+    protected List<BankAccountDetail> getBanksToPayTo() {
+
         return bankOptionToPayTo();
     }
 
     @Override
-    public List<BankDetail> getBanksToPayFrom() {
+    protected List<BankDetail> getBanksToPayFrom() {
 
         return bankOptionToPayFrom();
     }
+
+    @Override
+    public void requery(ShoppingCart shoppingCart) {
+        double decider = Math.random();
+        CartStatus newStatus = CartStatus.PENDING;
+        PaymentResponse.PaymentStatus paymentStatus = PaymentResponse.PaymentStatus.SUCCESSFUL;
+        if(decider <= 0.85){
+            paymentStatus = PaymentResponse.PaymentStatus.SUCCESSFUL;
+
+        }else if(decider <= 0.95){
+            paymentStatus = PaymentResponse.PaymentStatus.FAILED;
+        }else{
+            paymentStatus = PaymentResponse.PaymentStatus.PENDING;;
+        }
+        switch (paymentStatus){
+            case SUCCESSFUL-> {newStatus = CartStatus.PROCESSED;}
+            case FAILED -> {newStatus = CartStatus.FAILED;}
+            default -> {newStatus = CartStatus.PENDING;}
+        }
+
+
+        if(newStatus.equals(CartStatus.PROCESSED)){
+            shoppingCart.setDatePaid(new Date());
+        }
+
+        cartStateMachine.transition(shoppingCart, newStatus);
+
+    }
+
 }
